c43354e3295c402f9e6aa4c764fbbd653bc6b9b4
Krlg <karol.grulling@gmail.com>
2025-02-08 22:29:08 +0100

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = {
+  upload: {
+    config: {
+      providerOptions: {
+        localServer: {
+          mimeTypes: [
+            "audio/mpeg",
+            "audio/mp3",
+            "audio/wav",
+            "audio/ogg"
+          ],
+        },
+      },
+    },
+  },
+};
